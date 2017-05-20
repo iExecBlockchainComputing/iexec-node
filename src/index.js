e34d27cbe94387efb8cc9ca7eea6529524262493
Ishak LEGHLAM <ileghlam@e3r6p18.42.fr>
2017-05-20 23:58:21 +0200

    Ishak - new struct src/

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import registerServiceWorker from './constants/registerServiceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div></div>, document.getElementById('root'));
 registerServiceWorker();
