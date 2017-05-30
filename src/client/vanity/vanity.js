2a7ef42ef162d86220be1b3081b14a6f815cfa29
Ishak LEGHLAM <ileghlam@e2r1p11.42.fr>
2017-05-30 12:13:12 +0200

    Ishak - remove refacto

--- src/client/vanity/vanity.js
+++ src/client/vanity/vanity.js
@@ -1,14 +1,19 @@
+/* global EllipticCurve ECKey */
 const getECKeyFromAdding = (privKey1, privKey2) => {
   console.log('getECKeyFromAdding');
   const n = EllipticCurve.getSECCurveByName('secp256k1').getN();
   const ecKey1 = new ECKey(privKey1);
   const ecKey2 = new ECKey(privKey2);
   // if both keys are the same return null
-  if (ecKey1.getBitcoinHexFormat() === ecKey2.getBitcoinHexFormat()) return null;
+  if (ecKey1.getBitcoinHexFormat() === ecKey2.getBitcoinHexFormat()) {
+    return null;
+  }
   if (ecKey1 == null || ecKey2 == null) return null;
   const combinedPrivateKey = new ECKey(ecKey1.priv.add(ecKey2.priv).mod(n));
   // compressed when both keys are compresse
-  if (ecKey1.compressed && ecKey2.compressed) combinedPrivateKey.setCompressed(true);
+  if (ecKey1.compressed && ecKey2.compressed) {
+    combinedPrivateKey.setCompressed(true);
+  }
   return combinedPrivateKey;
 };
 
