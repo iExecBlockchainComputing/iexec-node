f27fa2fccde86a037c88bbc368ed50f9427e0b87
LODYGENSKY Oleg <lodygens@lal.in2p3.fr>
2017-07-26 15:30:52 +0200

    doc

--- docker/server-master/build.sh
+++ docker/server-master/build.sh
@@ -177,8 +177,10 @@ cat << EOF_INFO
   =================
   You can now create and start worker & client containers.
   Please open another terminal and copy :
-    - ${ROOTDIR}/xtremweb-hep-master/build/dist/xwhep-10.6.0/xwhep-worker-10.6.0.deb to docker/worker/ and build the worker container
-    - ${ROOTDIR}/xtremweb-hep-master/build/dist/xwhep-10.6.0/xwhep-client-10.6.0.deb to docker/client/ and build the client container
+    - ${ROOTDIR}/xwhep-server-10.6.0.deb to docker/server/ and build the worker container
+    - ${ROOTDIR}/xwhep-server-conf-10.6.0.deb to docker/server/ and build the worker container
+    - ${ROOTDIR}/xwhep-worker-10.6.0.deb to docker/worker/ and build the worker container
+    - ${ROOTDIR}/xwhep-client-10.6.0.deb to docker/client/ and build the client container
   =================
 
 EOF_INFO
