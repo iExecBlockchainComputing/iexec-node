090f99352595f6b3954c9ce6bf3762cafe61d81c
francois branciard <francois.branciard@iex.ec>
2017-08-17 16:51:01 +0200

    update for deploy vanity bridge in your local vagrant vm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bridge_vanity
+
+
+to deploy the vanity bridge in your local vagrant vm :
+
+```
+git checkout local-vagrant 
+```
+
+to see all the vanity local deployment see README here :
+https://github.com/iExecBlockchainComputing/vanitygen/tree/local-vagrant
--- vanitygen.js
+++ vanitygen.js
@@ -14,22 +14,22 @@ var exec = require('child_process').exec;
 //});
 
 
-if (typeof web3 !== 'undefined') {
-  web3 = new Web3(web3.currentProvider);
-} else {
+//if (typeof web3 !== 'undefined') {
+//  web3 = new Web3(web3.currentProvider);
+//} else {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
+//}
 
-web3.eth.defaultAccount="dc92a0556fe6586ad7193a2d147dcfef7a42db33";
+web3.eth.defaultAccount = web3.eth.accounts[0];
 console.log(web3.eth.defaultAccount);
 
 
 var vanitygenContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"userValue","type":"string"},{"name":"params","type":"string"}],"name":"setParam","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"userAddr","type":"address"},{"name":"keys","type":"string"},{"name":"url","type":"string"}],"name":"pushResult","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"},{"name":"_token","type":"address"},{"name":"_extraData","type":"string"},{"name":"_extraData2","type":"string"}],"name":"receiveApproval","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"launched","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"status","type":"string"},{"name":"user","type":"address"}],"name":"broadcast","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getResult","outputs":[{"name":"keyz","type":"string"},{"name":"url","type":"string"}],"payable":false,"type":"function"},{"inputs":[{"name":"_iextoken","type":"address"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"value","type":"string"},{"indexed":false,"name":"param","type":"string"},{"indexed":false,"name":"addr","type":"address"}],"name":"Launch","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"status","type":"string"},{"indexed":true,"name":"user","type":"address"}],"name":"Logs","type":"event"}]);
 
-var contract_adress = "0x45d3ccce031b9f84d082eb38be65dfa3ae642f85";
-var contractInstance = vanitygenContract.at(contract_adress);
+var contract_address ="0x65e1317709c7c320bf9b46c87761f87a08896de4";
+var contractInstance = vanitygenContract.at(contract_address);
 function submitTask( param, address) {
-	var task = '/home/ubuntu/run_vanitygen_with_replication.sh "' +  param + '"';
+	var task = '/home/vagrant/iexecdev/bridge_vanity/run_vanitygen_with_replication.sh "' +  param + '"';
 	console.log("submit TASK ", task);
 	var child = exec(task);
 	child.stdout.on('data', function(data) {
