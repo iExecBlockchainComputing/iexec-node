68b65b0226a4152cdc3ac96443ad64acff8cdd95
francois branciard <francois.branciard@iex.ec>
2017-09-13 21:31:32 +0200

    Full non reg 13/09/2017 OK see report.log

---
+++ tests/rf/gitcloneall.sh
@@ -0,0 +1,6 @@
+rm -rf xtremweb-hep
+git clone https://github.com/iExecBlockchainComputing/xtremweb-hep.git
+rm -rf iexec-oracle
+git clone https://github.com/iExecBlockchainComputing/iexec-oracle.git
+rm -rf iexec-bridge
+git clone https://github.com/iExecBlockchainComputing/iexec-bridge.git
