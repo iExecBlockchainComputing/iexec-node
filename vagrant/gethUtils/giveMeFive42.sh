84dd9d796caf796df17919a842c526c84936ca8d
francois branciard <francois.branciard@iex.ec>
2017-09-28 18:57:40 +0200

    IEXPROD-30 : test add user,stdout appNAme in callbacks

--- vagrant/gethUtils/giveMeFive42.sh
+++ vagrant/gethUtils/giveMeFive42.sh
@@ -3,4 +3,4 @@ if [ "$#" -ne 1 ]; then
     echo "usage: ./giveMeFive42.sh address "
     exit 1
 fi
-geth --exec "eth.sendTransaction({from:eth.coinbase, to:\"$1\", value: web3.toWei(5, \"ether\")})" attach ipc:/home/vagrant/iexecdev/.ethereum/net42/geth.ipc
+geth --exec "eth.sendTransaction({from:eth.coinbase, to:\"$1\", value: web3.toWei(5, \"ether\")})" attach http://localhost:8545
